89eb3c69192326359b6de53f7a2391c6e7923e2a
Igor <91311311+Galdenysh@users.noreply.github.com>
2021-12-08 20:53:44 +0300

    add js logic page navigation to 2 step

---
+++ scripts/infoOther.js
@@ -0,0 +1,17 @@
+const header = document.querySelector(".header");
+
+let prevScroll = window.scrollY;
+let curScroll;
+
+window.addEventListener("scroll", () => {
+  curScroll = window.scrollY;
+  const headerIsHidden = header.classList.contains("header_hidden");
+
+  if (curScroll > prevScroll && !headerIsHidden) {
+    header.classList.add("header_hidden");
+  } else if (curScroll < prevScroll && headerIsHidden) {
+    header.classList.remove("header_hidden");
+  }
+
+  prevScroll = curScroll;
+});
